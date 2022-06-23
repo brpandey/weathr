6d3eb6c841a81914ccfb58c7d64b4966f13a4104
Bibek Pandey <bibekp@gmail.com>
2022-06-22 22:42:07 -0600

    Undo mock using ureq request

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Use environment variable as source of api key via export WEATHER_API_KEY="mykey"
     dotenv().ok();
 
-//    let api_key: String = env::var("WEATHER_API_KEY").expect("export WEATHER_API_KEY=apikey not set");
-//    let url = format!("https://api.openweathermap.org/data/2.5/forecast?q=Many&nbsp;Farms&appid={}&units=imperial", api_key);
+    let api_key: String = env::var("WEATHER_API_KEY").expect("export WEATHER_API_KEY=apikey not set");
+    let url = format!("https://api.openweathermap.org/data/2.5/forecast?q=Many&nbsp;Farms&appid={}&units=imperial", api_key);
 
 //    dbg!(url);
 
-//    let response = ureq::get(&url).call().unwrap().into_string().unwrap();
+    let response = ureq::get(&url).call().unwrap().into_string().unwrap();
 //    dbg!(response);
 
-    let response = weathr::mock_json::JSON_RESPONSE;
+//    let response = weathr::mock_json::JSON_RESPONSE;
 
     let data: WeatherList = WeatherList::parse(&response)?;
     let forecast: WeatherForecast = data.into();
